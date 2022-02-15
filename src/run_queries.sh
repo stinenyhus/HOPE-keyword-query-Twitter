4fb15d03f28ba4d0a4adb4ad3da1442d47d6c394
StineNyhus <stnyl@hotmail.com>
2022-02-15 13:42:29 +0100

    Updated run-script, printing n tweets

--- src/run_queries.sh
+++ src/run_queries.sh
@@ -1,43 +1,57 @@
-DATE=0102
+DATE=1502
 
 # Omicron English tweets
-nohup bash pipeline.sh -k omicron-denmark &> logs/omicron-denmark_${DATE}.log &
+# bash pipeline.sh -k denmark &> logs/denmark_${DATE}.log &
+# echo "Finished running pipeline on denmark (i.e., English tweets)"
 
 # # Vaccine
-# nohup bash pipeline.sh -k vaccin &> logs/vaccin_${DATE}.log & 
+# bash pipeline.sh -k vaccin &> logs/vaccin_${DATE}.log 
+# echo "Finished running pipeline on vaccin"
 
 # # Coronapas
-# nohup bash pipeline.sh -k coronapas &> logs/coronapas_${DATE}.log & 
+bash pipeline.sh -k coronapas &> logs/coronapas_${DATE}.log
+echo "Finished running pipeline on coronapas"
 
 # # Mundbind
-# nohup bash pipeline.sh -k mundbind &> logs/mundbind_${DATE}.log & 
+bash pipeline.sh -k mundbind &> logs/mundbind_${DATE}.log 
+echo "Finished running pipeline on mundbind"
 
 # # Mette Frederiksen
-# nohup bash pipeline.sh -k mettef &> logs/mettef_${DATE}.log &
+# pipeline.sh -k mettef &> logs/mettef_${DATE}.log
+# echo "Finished running pipeline on mettef"
 
 # # Corona
-# nohup bash pipeline.sh -k corona &> logs/corona_${DATE}.log &
+# bash pipeline.sh -k corona &> logs/corona_${DATE}.log
+# echo "Finished running pipeline on corona"
 
 # # Restriktion
-# nohup bash pipeline.sh -k restriktion &> logs/restriktion_${DATE}.log &
+# bash pipeline.sh -k restriktion &> logs/restriktion_${DATE}.log
+# echo "Finished running pipeline on restriktion"
 
 # # Genåbning
-# nohup bash pipeline.sh -k genåb &> logs/genåb_${DATE}.log &
+# bash pipeline.sh -k genåb &> logs/genåb_${DATE}.log
+# echo "Finished running pipeline on genåb"
 
 # # Tvang
-# nohup bash pipeline.sh -k tvang &> logs/tvang_${DATE}.log &
+# bash pipeline.sh -k tvang &> logs/tvang_${DATE}.log 
+# echo "Finished running pipeline on tvang"
 
 # # Samfundskritisk
-# nohup bash pipeline.sh -k samfundskri &> logs/samfundskri_${DATE}.log &
+# bash pipeline.sh -k samfundskri &> logs/samfundskri_${DATE}.log 
+# echo "Finished running pipeline on samfundskri"
 
 # # Omicron
-# nohup bash pipeline.sh -k omicron &> logs/omicron_${DATE}.log &
+# bash pipeline.sh -k omicron &> logs/omicron_${DATE}.log 
+# echo "Finished running pipeline on omicron (Danish tweets)"
 
 # # Lockdown
-# nohup bash pipeline.sh -k lockdown &> logs/lockdown_${DATE}.log &
+# bash pipeline.sh -k lockdown &> logs/lockdown_${DATE}.log 
+# echo "Finished running pipeline on lockdown"
 
 # # Pressemøde
-# nohup bash pipeline.sh -k pressemøde &> logs/pressemøde_${DATE}.log &
+# bash pipeline.sh -k pressemøde &> logs/pressemøde_${DATE}.log 
+# echo "Finished running pipeline on pressemøde"
 
 # # 17 januar
-# nohup bash pipeline.sh -k 17jan &> logs/17jan_${DATE}.log &
+# bash pipeline.sh -k 17jan &> logs/17jan_${DATE}.log 
+# echo "Finished running pipeline on 17jan"
