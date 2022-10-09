2e163174c0b7c3d4e7d766b1581109848fa9d316
SaraMøllerØstergaard#1413 <sara.m.oestergaard@gmail.com>
2022-10-09 11:22:51 +0200

    monthly pipeline

--- src/weekly_pipeline.py
+++ src/weekly_pipeline.py
@@ -5,10 +5,13 @@ This script runs the hope analysis pipeline once a week
 import schedule
 import time
 import os
+from datetime import date
 
 # define the job: "run the defined bash script from terminal"
 def job():
-    os.system('bash run_queries.sh')
+    if date.today().day == 1:  # only do job on the first of the month
+        os.system("bash run_queries.sh")
+
 
 # schedule the job to run every Friday at 7 in the morning
 schedule.every().saturday.at("16:50").do(job)
@@ -16,4 +19,4 @@ schedule.every().saturday.at("16:50").do(job)
 # run the ongoing script to check every hour if it's 7 am yet
 while True:
     schedule.run_pending()
-    time.sleep(1) # 60 seconds * 60 mintues = 1 hour
+    time.sleep(1)  # 60 seconds * 60 mintues = 1 hour
